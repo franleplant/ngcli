91d4769a76ffeaacd63345ac4df65498453e20a9
franleplant <franleplant@gmail.com>
2014-12-13 00:28:21 -0300

    refactor and improve the code

--- bin/ngcli.js
+++ bin/ngcli.js
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var path = require('path');
-var swig = require('swig');
 var argv = require('argv');
 
 argv.version('v0.0.0');
-
 argv.info('info string!');
 
 argv.mod({
@@ -18,34 +14,25 @@ argv.mod({
         type: 'path',
         description: 'description blah blah'
 
+    }, {
+        name: 'module',
+        short: 'm',
+        type: 'string',
+        description: 'description blah blah'
+
     }]
 });
 
 
 var args = argv.run();
-
-
 console.log(args);
 
-if (args.mod === 'factory') {
 
-    factory({'moduleName': 'fucker', 'factoryName':args.targets[0] }, args.options.path)
+var locals = {
+    'moduleName': args.options.module || 'myModule',
+    'factoryName':args.targets[0]
 }
 
+var main = require('../index.js');
 
-function factory(locals, pathTo) {
-    var name = args.targets[0];
-    var suffix ='.service.js';
-    var suffixTest = '.service.spec.js';
-
-    var p = path.join(pathTo, name + suffix);
-    var pt = path.join(pathTo, name + suffixTest);
-
-
-
-    var a  = swig.renderFile(path.resolve(__dirname, '../templates/factory.service.js'), locals);
-    fs.writeFileSync(p, a)
-    a  = swig.renderFile(path.resolve(__dirname, '../templates/factory.service.spec.js'), locals);
-    fs.writeFileSync(pt, a)
-
-}
+main(args.mod, locals, args.options.path);
